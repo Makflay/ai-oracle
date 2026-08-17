6fcccc0b50014f85d7972ea08a41fcecf72ef1de
Viktor <chief.makflay1@gmail.com>
2026-08-17 10:21:27 +0300

    feat(client): configure Redux store

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import "./index.css";
+import { store } from "./app/store";
 import App from "./App.tsx";
 
 const rootElement = document.getElementById("root");
@@ -11,6 +13,8 @@ if (!rootElement) {
 
 createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 );
